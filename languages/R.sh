5ab3545a3c1eb98d21952198ed5e972e34962f1d
Matthew Galvin <matthewrgalvin@gmail.com>
2020-12-06 09:42:21 -0800

    Remove RStudio Server

--- languages/R.sh
+++ languages/R.sh
@@ -2,6 +2,3 @@
 echo "Installing R and RStudio..."
 
 sudo apt install r-base
-sudo apt-get install gdebi-core
-wget https://download2.rstudio.org/server/bionic/amd64/rstudio-server-1.3.1093-amd64.deb
-sudo gdebi rstudio-server-1.3.1093-amd64.deb
